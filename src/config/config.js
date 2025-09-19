4b25330b45097a67299c21fd3097144a055a4d51
inamkkkk <inamk9025@gmail.com>
2025-09-19 23:01:11 +0500

    fix: refine src/config/config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
+// Add other environment-dependent configs here
 module.exports = {
   PORT: process.env.PORT || 5000,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRE: process.env.JWT_EXPIRE,
+  JWT_EXPIRE: process.env.JWT_EXPIRE || '1d', // Default to 1 day if not set
   GEOCODE_API_KEY: process.env.GEOCODE_API_KEY,
-  // Add other environment-dependent configs here
 };
