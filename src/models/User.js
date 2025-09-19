3e4bbedbe6d0e0571c50e48b9dd4bd7f8cbd05a4
inamkkkk <inamk9025@gmail.com>
2025-09-19 23:02:12 +0500

    fix: refine src/models/User.js

--- src/models/User.js
+++ src/models/User.js
@@ -74,9 +74,11 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function (next) {
+  // Only hash the password if it has been modified
   if (!this.isModified('password')) {
-    next();
+    return next();
   }
+
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
