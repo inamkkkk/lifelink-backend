23a4be5cb6476fcb4a3213487eef8c8b232157f0
inamkkkk <inamk9025@gmail.com>
2025-09-19 23:01:48 +0500

    fix: refine src/utils/errorResponse.js

--- src/utils/errorResponse.js
+++ src/utils/errorResponse.js
@@ -6,10 +6,23 @@ class ErrorResponse extends Error {
    * Creates an instance of ErrorResponse.
    * @param {string} message - The error message.
    * @param {number} statusCode - The HTTP status code.
+   * @param {object} [details] - Optional additional details about the error.
    */
-  constructor(message, statusCode) {
+  constructor(message, statusCode, details) {
     super(message);
+
+    // Ensure the name of this error is the same as the class name
+    this.name = this.constructor.name;
+
     this.statusCode = statusCode;
+    this.details = details;
+
+    // Capturing the stack trace, excluding constructor call from it.
+    // This is a common practice for custom error classes in Node.js
+    // to ensure the stack trace is cleaner and points to the actual error origin.
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
 
